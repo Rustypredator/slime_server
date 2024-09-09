1d495666849ce8160f43a9ebf89c6a4c97151864
Rustypredator <contact@rusty.info>
2024-09-09 12:16:56 +0200

    testing around

--- source/bot_files/slime_bot.py
+++ source/bot_files/slime_bot.py
@@ -38,6 +38,8 @@ async def on_ready():
 @bot.event
 async def on_command(ctx):
     backend.set_discord_channel(ctx)
+    backend.send_msg(f"Command: {ctx.message.content}")
+    return #stop execution of command?
 
 async def setup(bot: commands.Bot) -> None:
     """
